7431b9c31406c91548ce2f7b445116ae2c3a43f4
carlostarazona <carlostarazona887@hotmail.com>
2020-09-21 07:44:33 -0500

    Ejm App Retrofit

---
+++ AppNetWorkingWX71/app/src/main/java/com/example/appnetworkingwx71/JokeActivity.kt
@@ -0,0 +1,47 @@
+package com.example.appnetworkingwx71
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_joke.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class JokeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_joke)
+
+        btJoke.setOnClickListener {
+            loadJoke()
+        }
+    }
+
+    private fun loadJoke() {
+        //tvJoke.text = "chiste...."
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://geek-jokes.sameerkumar.website/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val jokeService: JokeService
+        jokeService = retrofit.create(JokeService::class.java)
+
+        val request = jokeService.getJoke("json")
+
+        request.enqueue(object : Callback<Joke>{
+            override fun onResponse(call: Call<Joke>, response: Response<Joke>) {
+                if(response.isSuccessful){
+                    tvJoke.text = response.body()!!.joke
+                }
+            }
+
+            override fun onFailure(call: Call<Joke>, t: Throwable) {
+                Log.d("jokeActivity",t.toString())
+            }
+        })
+    }
+}
